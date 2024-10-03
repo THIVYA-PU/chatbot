0c2a511becdff81739cf8198cbec162f45dab1ea
Sample <you@example.com>
2024-10-03 19:15:30 +0530

    Handling Exception

--- chatbot.py
+++ chatbot.py
@@ -1,16 +1,19 @@
 import wikipediaapi
 wiki_obj = wikipediaapi.Wikipedia('English')
 def wiki_summary(query):
-    page = wiki_obj.page(query)
-    if page.exists():
-        return page.summary[:500]
-    else:
-        return "Sorry"
+    try:
+        page = wiki_obj.page(query)
+        if page.exists():
+            return page.summary[:500]
+        else:
+            return "Sorry Can't Find the wikipedia Page"
+    except Exception as e:
+        return f"An error Occured:{str(e)}"
 
 if __name__=="__main__":
     while(True):
-        inp=input("Enter:")
-        if inp=="exit":
+        inp=input("Enter the Title:")
+        if inp=="exit" or "quit":
             break
         else:
             res = wiki_summary(inp)
