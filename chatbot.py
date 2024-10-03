33b099047c4c51f0d14e152e83ab40309e1a7f5a
Sample <you@example.com>
2024-10-03 20:35:26 +0530

    Adding url for more reference

--- chatbot.py
+++ chatbot.py
@@ -4,7 +4,11 @@ def wiki_summary(query):
     try:
         page = wiki_obj.page(query)
         if page.exists():
-            return page.summary[:500]
+            res_summary = page.summary[:600]
+            words = len(res_summary.split())
+            link = page.fullurl
+            print(f"This summary contains {words} words\n")
+            return f"{res_summary}\n\nIf you want You can read more at: {link}"
         else:
             return "Sorry Can't Find the wikipedia Page"
     except Exception as e:
@@ -12,10 +16,17 @@ def wiki_summary(query):
 
 if __name__=="__main__":
     while(True):
-        inp=input("Enter the Title:")
-        if inp=="exit" or "quit":
+        try:
+            inp=input("Enter the Title:")
+            if inp=="exit" or inp == "quit":
+                break
+            else:
+                res = wiki_summary(inp)
+                print(f"{res}")
+        except KeyboardInterrupt:
+            print("Exiting")
             break
-        else:
-            res = wiki_summary(inp)
-            print(f"{res}")
+        except Exception as e:
+            print(f"Error Occured {str(e)}")
+            break
             
